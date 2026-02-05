4b103a1188e5d69fedc50b6442f74531aeaa4573
LancerFDa <rodrgolorenzo@gmail.com>
2026-02-05 10:12:33 +0100

    feat(symbol_zero): Pasar el caso test symbol_zero()
    Co-authored-by: FernanPC18 <FernanPC18@users.noreply.github.com>

--- src/scoreCard.py
+++ src/scoreCard.py
@@ -5,12 +5,18 @@ class ScoreCard:
 
     def frame_pins(self, pins):
         pins = self.pins
-        L = list(pins)
-        frames = {}
-        for i, j  in enumerate(L):
-            if i % 2 == 0:
-                frames[i // 2] = [int(j)]
-            else:
-                frames[i // 2].append(int(j))
-        return sum(sum(v) for v in frames.values())
-        
+        score = list(pins)
+        frames = []
+        for i in range(len(pins)):
+            if score[i] == "-":
+                score[i] = 0
+        for i in score:
+            frames.append(int(i))
+        total = sum(frames)
+        return total
+    
+
+if __name__=="__main__":
+    ScoreCard("9-9-9-9-9-9-9-9-9-9-").frame_pins()
+
+    
